2fade5c41fd09055cada3b399b46f6bc3470bacb
Hayfa alajmi <110268610+hayfa66@users.noreply.github.com>
2022-09-23 04:52:41 +0300

    Add files via upload

---
+++ app/run.py
@@ -0,0 +1,68 @@
+from flask import render_template
+from flask import Flask, request,jsonify
+from werkzeug.utils import secure_filename
+import os
+from glob import glob
+from utils.helper import *
+
+
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def human_or_dog(img_path):
+    '''
+    function to evaluate a photo if it has a dog or a human or neither
+    input : img path
+    output : breed of a dog name and a photo 
+    '''
+    if dog_detector(img_path):
+        dog_name = dog_breed(img_path)
+        return "This Dog breed is "+dog_name+"."
+    if face_detector(img_path):
+        dog_name = dog_breed(img_path)
+        return "This Photo looks like a "+dog_name+"."
+    return "No human or dog found"
+
+print(human_or_dog("MyPhotos/photo4.png"),"--------------------------------------------------------------------------------\n")
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('master.html')
+
+
+# route for prediction
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            img_file_path = os.path.join("app/uploads/", filename)
+            file.save(os.path.join("app/uploads/", filename))
+            # predict function
+            prediction = human_or_dog(img_file_path)
+            # delete uploaded file
+            os.remove(img_file_path)
+            return jsonify(prediction)
+        # return True
+
+    return ""
+if __name__ == '__main__':
+    '''
+        main run part
+    '''
+    server = '0.0.0.0'
+    port = '5000'
+    app.run(host = server,  port = port, debug = True)    
